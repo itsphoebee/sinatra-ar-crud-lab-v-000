6188270bb2325f9d34d388300f186a9fb76031f6
Phoebe Lau <phoebelau7@gmail.com>
2018-01-12 18:58:19 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -38,5 +38,7 @@ class ApplicationController < Sinatra::Base
   patch '/posts/:id' do
     @post = Post.find(params[:id])
     @post.update(name: params[:name], content: params[:content])
+
+    redirects '/posts/:id'
   end
 end
