5dee17c5131dec04440785e40d5e039076b24c04
Phoebe Lau <phoebelau7@gmail.com>
2018-01-12 18:57:36 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -35,4 +35,8 @@ class ApplicationController < Sinatra::Base
     erb :edit
   end
 
+  patch '/posts/:id' do
+    @post = Post.find(params[:id])
+    @post.update(name: params[:name], content: params[:content])
+  end
 end
